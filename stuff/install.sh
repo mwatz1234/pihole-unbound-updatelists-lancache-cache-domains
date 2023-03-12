5e626b7e994291667db1e9d481af105aa98563e2
mwatz1234 <87551985+mwatz1234@users.noreply.github.com>
2023-03-11 18:27:08 -0800

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -28,7 +28,7 @@ git clone https://github.com/uklans/cache-domains.git
 
 # Making copies of the files
 mkdir -p /etc/cache-domains/ && cp \`find ~/cache-domains -name *.txt -o -name cache_domains.json\` /etc/cache-domains
-mkdir -p /etc/cache-domains/scripts/ && cp ~/cache-domains/scripts/create-dnsmasq.sh /etc/cache-domains/scripts/
+mkdir -p /etc/cache-domains/scripts/ && cp ~/cache-domains/scripts/create-unbound.sh /etc/cache-domains/scripts/
 
 
 # Setting up our config.json file
@@ -44,14 +44,14 @@ sudo chmod -v 644 /etc/cache-domains/*
 sudo ln -s /etc/cache-domains/config/config.json /etc/cache-domains/scripts/config.json 
 
 # Make bash scripts executable
-sudo chmod -v +x /etc/cache-domains/scripts/create-dnsmasq.sh
+sudo chmod -v +x /etc/cache-domains/scripts/create-unbound.sh
 
 # Manually generating our dnsmasq files
 cd /etc/cache-domains/scripts
-./create-dnsmasq.sh
+./create-unbound.sh
 
 # Copying our files for Pi-hole to use 
-sudo cp -r /etc/cache-domains/scripts/output/dnsmasq/*.conf /etc/dnsmasq.d/
+sudo cp -r /etc/cache-domains/scripts/output/dnsmasq/*.conf /etc/unbound/unbound.conf.d/
 
 
 # Automating the process
